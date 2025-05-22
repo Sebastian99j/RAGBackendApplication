13ef867cf02b5f6120f16a9b9f015428c25643b8
Sebastian <sebastianmkj1@gmail.com>
2025-05-22 15:54:07 +0200

    Rename env name

--- core/redis_client.py
+++ core/redis_client.py
@@ -3,7 +3,7 @@ import json
 import os
 
 redis_host = os.getenv("REDIS_HOST", "localhost")
-redis_port = int(os.getenv("REDIS_PORT", 6379))
+redis_port = int(os.getenv("REDIS_CUSTOM_PORT", 6379))
 r = redis.Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 
